53ef54d17b9a070cd0690c35c8676047b68c8b25
BKJang <jp302119@gmail.com>
2018-10-17 11:43:38 +0900

    :house: BKJang / feat / 최초 커밋 테스트

--- front/src/component/App/index.jsx
+++ front/src/component/App/index.jsx
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>front</p>
+          <p>IHOME FrontEnd</p>
         </header>
       </div>
     );
