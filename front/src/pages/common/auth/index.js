7dd573077a99ccea0f6339248b4d23336a937326
BKJang <jp302119@gmail.com>
2018-10-18 11:40:11 +0900

    :house: BKJang / feat / 리덕스 모듈 연동

--- front/src/pages/common/auth/index.js
+++ front/src/pages/common/auth/index.js
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 export { default as Login } from './Login';
+=======
+export { default as Login } from './Login';
+
+
+
+>>>>>>> 96fe8d4... :house: BKJang / feat / 리덕스 모듈 연동
---
+++ front/src/store/index.js
@@ -0,0 +1,3 @@
+import configureStore from '../redux/configureStore';
+
+export default configureStore();
