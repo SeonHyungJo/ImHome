21acd451a1875f7c431237e9a7f1c1ca73108af2
SeonHyungJo <seonhyung.jo@gmail.com>
2018-10-20 00:12:14 +0900

    :house: seonhyungjo / feat / Router, Mongoose 연동 및 샘플 작성

--- server/app.js
+++ server/app.js
@@ -1,11 +1,37 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
+var mongoose = require('mongoose');
+require('dotenv').config();
+
 var app = express();
+
+var port = process.env.PORT || 3000;
+
+// CONNECT TO MONGODB SERVER
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+// DEFINE MODEL
+var Books = require('./models/books');
 
 app.get("/", (req, res) => {
     res.send({ hello: "world" });
 });
 
-var port = process.env.PORT || 3000; //*
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+// GET ALL BOOKS
+app.get('/books', function(req,res){
+  Books.find(function(err, books){
+    if(err){
+      return res.status(500).send({error: 'database failure'});
+    } 
+
+    res.json(books);
+  });
+});
+
 app.listen(port, function(){
-  console.log('Server On!');
+  console.log('Node app is running on port', port);
 });
