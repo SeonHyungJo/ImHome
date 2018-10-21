6af39520dbed78de99c345675ddc1651d0e6fc82
SeonHyung <seonhyung.jo@gmail.com>
2018-10-22 01:07:10 +0900

    Server seonhyungjo (#11) (#12)
    * :house: seonhyungjo / feat / Router, Mongoose 연동 및 샘플 작성
    
    * :house: seonhyungjo / feat / DB연결 And JWT setting
    
    * :house: seonhyungjo / feat / MongoDB Schema Setting
    
    * :house: seonhyungjo / feat / Router Setting
    
    * :house: seonhyungjo / feat / JWT, Router, DBd연동 적용

---
+++ server/router/api/user.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var Users = require('../../models/users');
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.get('/', function (req, res) {
+    Users.find(function (err, users) {
+        if (err) {
+            return res.status(500).send({ error: 'database failure' });
+        }
+        res.json(users);
+    });
+});
+
+router.post('/', (req, res) => {
+    Users.create(req.body)
+        .then(todo => res.send(todo))
+        .catch(err => res.status(500).send(err));
+});
+
+// GET by branchCode
+router.get('/user/:id', function (req, res) {
+    Users.findOneById(req.params.id)
+        .then((user) => {
+            if (!user) return res.status(404).send({ err: 'user not found' });
+            console.log(user);
+            res.json(user);
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+// Update by branchCode
+router.put('/user/:id', (req, res) => {
+    Users.updateById(req.params.id, req.body)
+        .then(user => res.send(user))
+        .catch(err => res.status(500).send(err));
+});
+
+// Delete by branchCode
+router.delete('/user/:id', (req, res) => {
+    Users.deleteById(req.params.id)
+        .then(() => res.sendStatus(200))
+        .catch(err => res.status(500).send(err));
+});
+
+module.exports = router;
