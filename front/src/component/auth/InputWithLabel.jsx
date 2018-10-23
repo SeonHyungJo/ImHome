f23cfa65f8789d6cab9e9fc3f5daa68a17e279e4
BKJang <jp302119@gmail.com>
2018-10-23 12:57:09 +0900

    :house: BKJang/ feat / Button 및 Input 컴포넌트 구현

---
+++ front/src/component/auth/InputWithLabel.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// 두개가 함께 있을땐 상단 (그 사이) 에 여백을 준다
+const Wrapper = styled.div`
+    & + & {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+`;
+
+const Label = styled.label`
+    font-size: 0.8rem;
+    color: #000000;
+    margin-bottom: 0.25rem;
+    margin-right: 4rem;
+`;
+
+const Input = styled.input`
+    width: 50%;
+    border: 1px solid #c2c2c2;
+    outline: none;
+    border-radius: 3px;
+    height: 1.4em;
+    font-size: 1.2rem;
+    padding-left: 0.5rem;
+    padding-right: 0.01rem;
+    ::placeholder {
+        color: #7d7d7d;
+    }
+`;
+
+// rest 쪽에는 onChange, type, name, value, placeholder 등의 input 에서 사용 하는 값들을 넣어줄수 있다.
+const InputWithLabel = ({label, ...rest}) => (
+    <Wrapper>
+        <Label>{label}</Label>
+        <Input {...rest}/>
+    </Wrapper>
+);
+
+export default InputWithLabel;
--- front/src/component/auth/index.js
+++ front/src/component/auth/index.js
@@ -1,2 +1,4 @@
 export { default as RegisterWrapper } from './RegisterWrapper';
+export { default as InputWithLabel } from './InputWithLabel';
+export { default as Button } from './Button';
 
