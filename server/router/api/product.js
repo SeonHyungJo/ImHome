44720a34d27c9ab3ce8e3aa2d1393d8c580b2cf5
SeonHyungJo <seonhyung.jo@gmail.com>
2018-10-23 21:15:37 +0900

    :house: seonhyungjo / feat / Product Router 추가

--- server/router/api/product.js
+++ server/router/api/product.js
@@ -1,30 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('../../models/users');
+var Products = require('../../models/Products');
 
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
 
-router.get('/user', function (req, res) {
-    Users.find(function (err, users) {
+router.get('/product', function (req, res) {
+    Products.find(function (err, productList) {
         if (err) {
             return res.status(500).send({ error: 'database failure' });
         }
-        res.json(users);
+        res.json(productList);
     });
 });
 
-router.post('/user', (req, res) => {
-    Users.create(req.body)
+router.post('/product', (req, res) => {
+    Products.create(req.body)
         .then(todo => res.send(todo))
         .catch(err => res.status(500).send(err));
 });
 
 // GET by branchCode
-router.get('/user/:id', function (req, res) {
-    Users.findOneById(req.params.id)
+router.get('/product/:id', function (req, res) {
+    Products.findOneById(req.params.id)
         .then((user) => {
             if (!user) return res.status(404).send({ err: 'user not found' });
             console.log(user);
@@ -34,15 +34,15 @@ router.get('/user/:id', function (req, res) {
 });
 
 // Update by branchCode
-router.put('/user/:id', (req, res) => {
-    Users.updateById(req.params.id, req.body)
+router.put('/product/:id', (req, res) => {
+    Products.updateById(req.params.id, req.body)
         .then(user => res.send(user))
         .catch(err => res.status(500).send(err));
 });
 
 // Delete by branchCode
-router.delete('/user/:id', (req, res) => {
-    Users.deleteById(req.params.id)
+router.delete('/product/:id', (req, res) => {
+    Products.deleteById(req.params.id)
         .then(() => res.sendStatus(200))
         .catch(err => res.status(500).send(err));
 });
