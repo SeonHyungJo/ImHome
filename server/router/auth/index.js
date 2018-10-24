e20d856e2227d284ecb0ceb0868254611934ac78
SeonHyungJo <seonhyung.jo@gmail.com>
2018-10-24 20:08:40 +0900

    :house: seonhyungjo / feat / JWT Loign 및 /check구현

--- server/router/auth/index.js
+++ server/router/auth/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const controller = require('./auth.controller')
 
+//router.post('/register', controller.register)
 router.post('/login', controller.login)
+
+//router.use('/check', authMiddleware)
+router.get('/check', controller.check)
 
 module.exports = router
