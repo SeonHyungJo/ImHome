44cf2ca3dbd787b145f7aae5a3248eb6e00b68b7
SeonHyungJo <seonhyung.jo@gmail.com>
2018-10-24 21:23:27 +0900

    :house: seonhyungjo / feat / Product 관련 수정진행

--- server/models/products.js
+++ server/models/products.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const productsSchema = new Schema({
-  companyName: { type: String, required: true },
-  companyCode: { type: String, required: true },
-  items: [{
-    itemCode: { type: String, required: true, unique: true },
-    itemName: { type: String, required: true },
-    itemCount: { type: String, required: true },
-    itemCost: { type: String, required: true },
-    itemVolume: { type: String, required: true },
-    itemDepth: { type: String, required: true },
-  }],
+const itemsSchema = new Schema({
+  itemCode: { type: String, required: true, unique: true },
+  itemName: { type: String, required: true },
+  itemCount: { type: String, required: true },
+  itemCost: { type: String, required: true },
+  itemVolume: { type: String, required: true },
+  itemDepth: { type: String, required: true },
 },
 {
-  timestamps: true
+  sparse: true,
+  _id: false
+});
+
+const productsSchema = new Schema({
+  companyName: { type: String, required: true },
+  companyCode: { type: String, required: true, unique: true },
+  items: [itemsSchema],
+},
+{
+  timestamps: true,
 });
 
 productsSchema.statics.create = function (payload) {
   const product = new this(payload);
-  console.log(product);
   return product.save();
 };
 
@@ -27,8 +32,8 @@ productsSchema.statics.findAll = function () {
   return this.find({});
 };
 
-productsSchema.statics.findOneById = function (id) {
-  return this.find({ id: id });
+productsSchema.statics.findOneByCompanyCode = function (companyCode) {
+  return this.find({ companyCode });
 };
 
 productsSchema.statics.findOneById = function (id, payload) {
--- server/router/api/product.js
+++ server/router/api/product.js
@@ -17,14 +17,23 @@ router.get('/product', function (req, res) {
 });
 
 router.post('/product', (req, res) => {
-    Products.create(req.body)
-        .then(todo => res.send(todo))
+    Products.findOneByCompanyCode(req.body.companyCode)
+        .then((product) => {
+            console.log(product.length);
+            if(product.length == 0){
+                Products.create(req.body)
+                    .then(result => res.send(result))
+                    .catch(err => res.status(500).send(err));
+            }else{
+                res.status(404).send({ err: 'already exit' });
+            }
+        })
         .catch(err => res.status(500).send(err));
 });
 
 // GET by branchCode
-router.get('/product/:id', function (req, res) {
-    Products.findOneById(req.params.id)
+router.get('/product/:companyCode', function (req, res) {
+    Products.findOneById(req.params.companyCode)
         .then((user) => {
             if (!user) return res.status(404).send({ err: 'user not found' });
             console.log(user);
@@ -34,15 +43,15 @@ router.get('/product/:id', function (req, res) {
 });
 
 // Update by branchCode
-router.put('/product/:id', (req, res) => {
-    Products.updateById(req.params.id, req.body)
+router.put('/product/:companyCode', (req, res) => {
+    Products.updateById(req.params.companyCode, req.body)
         .then(user => res.send(user))
         .catch(err => res.status(500).send(err));
 });
 
 // Delete by branchCode
-router.delete('/product/:id', (req, res) => {
-    Products.deleteById(req.params.id)
+router.delete('/product/:companyCode', (req, res) => {
+    Products.deleteById(req.params.companyCode)
         .then(() => res.sendStatus(200))
         .catch(err => res.status(500).send(err));
 });
