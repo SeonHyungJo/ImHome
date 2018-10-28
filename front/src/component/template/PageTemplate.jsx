deff9db4f8ef03eef109c209d440844e97ae2ee5
PaulJ <jp302119@gmail.com>
2018-10-28 11:53:11 +0900

    :house: BKJang/ feat/ Header화면 퍼블리싱 구현

--- front/src/component/template/PageTemplate.jsx
+++ front/src/component/template/PageTemplate.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Header from '../header/HeaderContainer';
 import Nav from '../nav/NavContainer';
+import { NavLink } from 'react-router-dom';
+
 
 class PageTemplate extends Component {
 
     render() {
         return (
             <div>
-                <Header />
-                    {this.props.children}
+                <Header>
+                    <li><NavLink to="/register" activeClassName="on">로그아웃</NavLink></li>
+                    <li><NavLink to="/admin/users" activeClassName="on">회원정보</NavLink></li>
+                    <li><NavLink to="#">출고내역조회</NavLink></li>
+                    <li><NavLink to="#">주문내역조회</NavLink></li>
+                    <li><NavLink to="#">품목관리</NavLink></li>
+                </Header>
                 <Nav />
+                {this.props.children}
+                
             </div>
         );
     }
