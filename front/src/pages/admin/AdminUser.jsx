420d1f53dd45d53a5cd2a66438fa2c353e2e49d4
PaulJ <jp302119@gmail.com>
2018-10-30 00:40:50 +0900

    :house: BKJang / feat/ common 컴포넌트 분리

--- front/src/pages/admin/AdminUser.jsx
+++ front/src/pages/admin/AdminUser.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { PageTemplate } from '../../component/template';
+import { TableWithScroll } from '../../component/table';
 
 class AdminMain extends Component {
     constructor() {
         super();
+        const headerData = [
+            { id: "registDate", numeric: false, disablePadding: true, label: '등록일자'},
+            { id: "storeName", numeric: false, disablePadding: true, label: '지점명' },
+            { id: "custName", numeric: false, disablePadding: true, label: '대표자' },
+            { id: "custId", numeric: false, disablePadding: true, label: '아이디' },
+            { id: "businessNum", numeric: false, disablePadding: true, label: '사업자번호' },
+            { id: "totalMoney", numeric: true, disablePadding: true, label: '총 거래금액(원)' }
+        ];
         this.state = {
             navData: [
                 {id: 1, name: '분당점'},
@@ -12,18 +21,18 @@ class AdminMain extends Component {
                 {id: 4, name: '이대본점'},
                 {id: 5, name: '용인죽전점'}
             ],
-            storeId : 1
+            storeId : 1,
+            headerData: headerData,
+            data: [
+                { registDate: '2018-10-29', storeName: '분당점', custName: '최장길', custId: 'imhome', businessNum: '426-50-00326', totalMoney: 10000500 }
+            ],
         };
     }
 
     render() {
         return (
             <PageTemplate navData={this.state.navData} storeId={this.state.storeId}>
-                <div className="App">
-                    <header className="App-header">
-                        <p>AdminUser</p>
-                    </header>
-                </div>
+                <TableWithScroll headerData={this.state.headerData} data={this.state.data} gridTitle="회원목록 및 정보" />
             </PageTemplate>
         );
     }
