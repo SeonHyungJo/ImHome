91177df16aad935919b2450c8aa628355f27fae4
PaulJ <jp302119@gmail.com>
2018-10-29 21:42:53 +0900

    :house: BKJang/ feat / 좌측 Navigator  컴포넌트 구현

--- front/src/pages/admin/AdminUser.jsx
+++ front/src/pages/admin/AdminUser.jsx
@@ -4,15 +4,26 @@ import { PageTemplate } from '../../component/template';
 class AdminMain extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            navData: [
+                {id: 1, name: '분당점'},
+                {id: 2, name: '백현점'},
+                {id: 3, name: '광주탄천점'},
+                {id: 4, name: '이대본점'},
+                {id: 5, name: '용인죽전점'}
+            ],
+            storeId : 1
+        };
     }
 
     render() {
         return (
-            <PageTemplate>
-                <div  style={{backgroundColor: 'black'}}>
-                    <p>AdminUser</p>
-                </div>        
+            <PageTemplate navData={this.state.navData} storeId={this.state.storeId}>
+                <div className="App">
+                    <header className="App-header">
+                        <p>AdminUser</p>
+                    </header>
+                </div>
             </PageTemplate>
         );
     }
