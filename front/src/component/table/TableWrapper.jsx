df64163a82602e2df56e5203b22090698026b759
PaulJ <jp302119@gmail.com>
2018-10-30 00:41:16 +0900

    :house: BKJang / feat/ 테이블 공통 컴포넌트 구현(수정 필요)

---
+++ front/src/component/table/TableWrapper.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background: white;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    width: 84rem;
+    float: right;
+    margin-right: 2.3rem;
+`;
+
+const Title = styled.h4`
+    margin-bottom: 20px;     
+`
+
+const TableWrapper = ({ title, children }) => (
+    <Wrapper>
+        <Title>{title}</Title>
+        {children}
+    </Wrapper>
+);
+
+export default TableWrapper;
