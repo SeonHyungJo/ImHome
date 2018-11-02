7d0d04e04df3cd8de715834114938974fdddfa98
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-02 22:43:49 +0900

    :house: seonhyungjo / fix / Test

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-//test
+//test3
 const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./router/api/user');
@@ -11,28 +11,6 @@ const morgan = require('morgan')
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// 보안을 위한 helmet 라이브러리 사용
-app.use(helmet());
-// X-Powered-By 헤더는 사용하지 않도록 설정
-app.disable('x-powered-by');
-
-//기본 세션 쿠키 이름을 사용하지 않음
-// var expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // 1 hour
-// app.use(session({
-//   name: 'session',
-//   keys: ['key1', 'key2'],
-//   cookie: { secure: true,
-//             httpOnly: true,
-//             domain: 'example.com',
-//             path: 'foo/bar',
-//             expires: expiryDate
-//           }
-//   })
-// );
-
-// set the secret key variable for jwt
-app.set('jwt-secret', process.env.secret)
 
 // Body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
