294b8f91d25c74beb766f2290101510630936856
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-16 00:10:45 +0900

    :house: seonhyungjo / test / navlink TEST 구현

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,38 +1,79 @@
-import React, { Component } from 'react';
-import './App.scss';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { UserMain, UserRegister } from './pages/user';
-import { AdminMain, AdminUser } from './pages/admin';
-import { OrderList } from './pages/orderList';
-import { Login } from './pages/common';
+import React, { Component } from 'react'
+import './App.scss'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom'
+import { UserMain, UserRegister } from './pages/user'
+import { AdminMain, AdminUser } from './pages/admin'
+import { OrderList } from './pages/orderList'
+import HeaderContainer from './component/header/HeaderContainer'
+import { Login } from './pages/common'
 
 class App extends Component {
+    render() {
+        const PATH = '/admin'
+        const { store } = this.props
 
-  render() {
-    const PATH = '/admin';
-    const { store } = this.props;
-
-    return (
-      <Provider store={store}>
-        <div>
-          <BrowserRouter>
-            <div>
-              {/* Admin router : 관리자 라우터*/}
-              <Route exact={true} component={AdminMain} path={PATH} />
-              <Route exact={true} component={AdminUser} path={PATH + '/users'} />
-              <Route exact={true} component={OrderList} path={PATH + '/orderlist'} />
-              {/* User router : 사용자 라우터*/}
-              <Route exact={true} component={UserMain} path='/' />
-              <Route exact={true} component={UserRegister} path='/register' />
-              {/* Common router : 관리자와 사용자 공통 라우터*/}
-              <Route component={Login} path='/login' />
-            </div>
-          </BrowserRouter>
-        </div>
-      </Provider>
-    );
-  }
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        {/* Admin router : 관리자 라우터*/}
+                        <Route path={PATH} render={() => <TestMainComponent path={PATH} />} />
+                        {/* User router : 사용자 라우터*/}
+                        <Route exact={true} component={UserMain} path="/" />
+                        <Route component={UserRegister} path="/register" />
+                        {/* Common router : 관리자와 사용자 공통 라우터*/}
+                        <Route component={Login} path="/login" />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        )
+    }
 }
 
-export default App;
+class TestMainComponent extends Component {
+    render() {
+        return (
+            <>
+                <HeaderContainer>
+                    <li>
+                        <NavLink to="/register" activeClassName="on">
+                            로그아웃
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin/users" activeClassName="on">
+                            회원정보
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="#">출고내역조회</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin/orderlist" activeClassName="on">
+                            주문내역조회
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="#">품목관리</NavLink>
+                    </li>
+                </HeaderContainer>
+
+                <div>
+                    <Switch>
+                        {/* Admin router : 관리자 라우터*/}
+                        <Route
+                            exact={true}
+                            render={() => <div>Welcome Admin Main</div>}
+                            path={'/admin'}
+                        />
+                        <Route exact={true} component={AdminUser} path={'/admin/users'} />
+                        <Route exact={true} component={OrderList} path={'/admin/orderlist'} />
+                    </Switch>
+                </div>
+            </>
+        )
+    }
+}
+
+export default App
--- front/src/component/template/PageTemplate.jsx
+++ front/src/component/template/PageTemplate.jsx
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
-import HeaderContainer from '../header/HeaderContainer';
-import { NavLink } from 'react-router-dom';
-import { NavTemplate } from '../nav';
-import Content from '../content/ContentContainer';
-
+import React, { Component } from 'react'
+import HeaderContainer from '../header/HeaderContainer'
+import { NavLink } from 'react-router-dom'
+import { NavTemplate } from '../nav'
+import Content from '../content/ContentContainer'
 
 class PageTemplate extends Component {
-
     render() {
         return (
             <div>
-                <HeaderContainer>
+                {/* <HeaderContainer>
                     <li><NavLink to="/register" activeClassName="on">로그아웃</NavLink></li>
                     <li><NavLink to="/admin/users" activeClassName="on">회원정보</NavLink></li>
                     <li><NavLink to="#">출고내역조회</NavLink></li>
                     <li><NavLink to="/admin/orderlist" activeClassName="on">주문내역조회</NavLink></li>
                     <li><NavLink to="#">품목관리</NavLink></li>
-                </HeaderContainer>
+                </HeaderContainer> */}
                 <NavTemplate navData={this.props.navData} storeId={this.props.storeId} />
-                <Content>
-                    {this.props.children}
-                </Content>
+                <Content>{this.props.children}</Content>
             </div>
-        );
+        )
     }
 }
 
-export default PageTemplate;
+export default PageTemplate
