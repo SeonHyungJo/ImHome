ccbb97662469b0f0e2a02b4fb62445047f476fab
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-21 23:28:49 +0900

    :house: seonhyungjo / fix / Nav Custom Scroll 추가

--- front/src/component/nav/NavContainer.jsx
+++ front/src/component/nav/NavContainer.jsx
@@ -2,55 +2,69 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-    background-color: white;
-    min-height: 89.8vh;
-    border-bottom: 1px dashed #c2c2c2;
-    width: 17rem;
     position: relative;
+    background-color: white;
+    background-position: 0 0;
     color: black;
     text-align: center;
-    z-index : 3;  
-    float: left;  
-    background-position: 0 0; 
-`
+    float: left;
+    overflow-y: scroll;
+    width: 17rem;
+    max-height: 89.8vh;
+    z-index: 3;
+
+    /* Scroll Custom */
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-button {
+        width: 0px;
+        height: 0px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: #fe4c8d;
+        border-radius: 8px;
+    }
+`;
 
 const NavWrapper = styled.ul`
-    width:13rem;
-    background:white;
-    text-align:left;
-    list-style:none;
-    padding-top:0.5rem;
-    li{
+    background: white;
+    text-align: left;
+    list-style: none;
+    padding-top: 0.5rem;
+    width: 13rem;
+
+    li {
         box-sizing: border-box;
         padding: 1.4rem 0.5rem 0.5rem 2.5rem;
-        font-size:1.1rem;
-        color:#555555;
-        font-weight:bold;
-        text-decoration:none;
+        font-size: 1.1rem;
+        color: #555555;
+        font-weight: bold;
+        text-decoration: none;
         background-image: url('/images/icon_b.png');
         background-repeat: no-repeat;
         background-position: 8% 70%;
         background-size: 1.1rem;
-        border-bottom: 1px solid #d7d7d7
-    };
+        border-bottom: 1px solid #d7d7d7;
+    }
     li:hover {
-        background-color:#fe4c8d;
+        background-color: #fe4c8d;
         color: #ffffff;
-        background-image:url('/images/icon_w.png')
-    };
+        background-image: url('/images/icon_w.png');
+    }
     .on {
-        background-color:#fe4c8d;
+        background-color: #fe4c8d;
         color: #ffffff;
-        background-image:url('/images/icon_w.png');
+        background-image: url('/images/icon_w.png');
         background-repeat: no-repeat;
     }
-`
+`;
 
 const NavContainer = ({ children }) => (
     <Wrapper>
-        <NavWrapper>
-            {children}
-        </NavWrapper>
+        <NavWrapper>{children}</NavWrapper>
     </Wrapper>
 );
 
