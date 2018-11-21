9cf6fe642e3456f83411a6b3cb2c9b98f98fec72
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-22 00:48:28 +0900

    :house: seonhyungjo / fix / Semi Colon수정

--- front/src/component/common/Button.jsx
+++ front/src/component/common/Button.jsx
@@ -6,22 +6,18 @@ const Wrapper = styled.button`
     padding-top: 0.5rem;
     padding-bottom: 0.5rem;
 
-    border : 2px solid #fe4c8d
+    border: 2px solid #fe4c8d;
     border-radius: 3px;
     background: white;
-    color : #fe4c8d
+    color: #fe4c8d;
 
     text-align: center;
     font-size: 0.8rem;
     width: 8rem;
     cursor: pointer;
-    font-weight: bold
+    font-weight: bold;
 `;
 
-const Button = ({children, onClick}) => (
-    <Wrapper onClick={onClick}>
-        {children}
-    </Wrapper>
-);
+const Button = ({ children, onClick }) => <Wrapper onClick={onClick}>{children}</Wrapper>;
 
 export default Button;
