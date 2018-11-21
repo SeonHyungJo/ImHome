cb27a7a8a549bde3f5df81229c3218cfc4c2fa72
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-22 00:49:54 +0900

    :house: seonhyungjo / fix / Div -> Fragment처리(영역처리로 인해)

--- front/src/component/nav/NavTemplate.jsx
+++ front/src/component/nav/NavTemplate.jsx
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
 import { NavContainer } from './';
 
-
 class NavTemplate extends Component {
-
     render() {
         return (
-            <div>
+            <>
                 <NavContainer>
-                    {
-                        this.props.navData && this.props.navData.map((data, index) => {
-                            return (
-                                data.id === this.props.storeId ?
-                                    <li className='on' key={index} id={data.id}>
-                                        {data.name}
-                                    </li>
-                                    :
-                                    <li key={index} id={data.id}>
-                                        {data.name}
-                                    </li>
+                    {this.props.navData &&
+                        this.props.navData.map((data, index) => {
+                            return data.id === this.props.storeId ? (
+                                <li className="on" key={index} id={data.id}>
+                                    {data.name}
+                                </li>
+                            ) : (
+                                <li key={index} id={data.id}>
+                                    {data.name}
+                                </li>
                             );
-                        })
-                    }
+                        })}
                 </NavContainer>
-            </div>
+            </>
         );
     }
 }
