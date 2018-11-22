ea695b9b55d5c992990494170251f6e2a22c2a75
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-23 00:00:16 +0900

    :house: seonhyungjo / fix / Fix

--- front/src/component/specificationTable/SpecificationTable.jsx
+++ front/src/component/specificationTable/SpecificationTable.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import classNames from 'classnames';
 
 const ContentWrapper = styled.div`
     display: inline-block;
@@ -8,7 +7,7 @@ const ContentWrapper = styled.div`
     padding: 10px;
 `;
 
-const SpecificationTable = ({ branchName, orderList, buttonList }) => (
+const SpecificationTable = () => (
     <ContentWrapper>
         <div>명세서 23일까지</div>
     </ContentWrapper>
