89cf252c277640bcde3d24656374a761404e554e
SeonHyungJo <seonhyung.jo@gmail.com>
2018-12-05 00:50:59 +0900

    :house: seonhyungjo / refactor / Order 관련 CRUD 생성

--- server/models/orders.js
+++ server/models/orders.js
@@ -1,48 +1,90 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const itemsSchema = new Schema({
-  itemCode: { type: String, required: true },
-  itemName: { type: String, required: true },
-  itemCount: { type: String, required: true },
-  itemCost: { type: String, required: true },
-  itemVolume: { type: String, required: true },
-  itemDepth: { type: String, required: true },
-},
-{
-  sparse: true,
-  _id: false
-});
+const itemsSchema = new Schema(
+    {
+        itemCode: { type: String, required: true },
+        itemName: { type: String, required: true },
+        itemCount: { type: String, required: true },
+        itemCost: { type: String, required: true },
+        itemVolume: { type: String, required: true },
+        itemDepth: { type: String, required: true }
+    },
+    {
+        sparse: true,
+        _id: false
+    }
+);
 
-const orderSchema = new Schema({
-  branchName: { type: String, required: true },
-  branchCode: { type: String, required: true },
-  items: [itemsSchema],
-  complete: { type: Boolean, default: false },
-},
-{
-  timestamps: true,
-});
+const orderSchema = new Schema(
+    {
+        orderId: { type: String, required: true },
+        branchName: { type: String, required: true },
+        branchCode: { type: String, required: true },
+        items: [itemsSchema],
+        complete: { type: Boolean, default: false }
+    },
+    {
+        timestamps: true
+    }
+);
 
-orderSchema.statics.create = function (payload) {
-  const product = new this(payload);
-  return product.save();
+/**
+ * @author seonhyungjo
+ * @summary 주문내역 생성
+ * @private
+ * @memberof Admin
+ * @param payload : Json형식 데이터
+ * @see None
+ * @returns «Query»
+ */
+orderSchema.statics.create = function(payload) {
+    const product = new this(payload);
+    return product.save();
 };
 
-orderSchema.statics.findAll = function () {
-  return this.find({});
+/**
+ * @author seonhyungjo
+ * @summary 출고완료되지 않은 주문내역 가져오기
+ * @private
+ * @memberof Admin
+ * @param branchCode : 지점코드로 구분
+ * @see None
+ * @returns «Query»
+ */
+orderSchema.statics.findInCompleteOrder = function(branchCode) {
+    return this.find({ branchCode, complete: false });
 };
 
-orderSchema.statics.findOneByBranchCode = function (branchCode) {
-  return this.find({ branchCode, complete: false });
+/**
+ * @author seonhyungjo
+ * @summary 주문내역 업데이트
+ * @private
+ * @memberof Admin
+ * @param branchCode : 지점코드로 구분
+ * @param productInfo : 수정하는 Json형식 데이터
+ * @see None
+ * @returns «Query»
+ */
+orderSchema.statics.findOneAndUpdateNew = function(branchCode, productInfo) {
+    return this.findOneAndUpdate({ branchCode, complete: false }, productInfo, { new: true });
 };
 
-orderSchema.statics.findOneAndUpdateNew = function (branchCode, productInfo) {
-  return this.findOneAndUpdate({ branchCode }, productInfo, { new: true });
+/**
+ * @author seonhyungjo
+ * @summary Complete = true로 변경 진행(출고완료처리)
+ * @private
+ * @memberof AdminchangeCompleteTrue
+ * @param branchCode : 지점코드로 구분
+ * @see None
+ * @returns «Query»
+ */
+orderSchema.statics.changeCompleteTrue = function(branchCode) {
+    return this.findOneAndUpdate({ branchCode, complete: false }, { complete: true });
 };
 
-orderSchema.statics.deleteByBranchCode = function (branchCode) {
-  return this.remove({ branchCode });
+orderSchema.statics.deleteByBranchCode = function(branchCode) {
+    return this.remove({ branchCode });
 };
 
 module.exports = mongoose.model('orders', orderSchema);
--- server/router/api/order.js
+++ server/router/api/order.js
@@ -1,63 +1,131 @@
 import express from 'express';
 import mongoose from 'mongoose';
 const Orders = require('../../models/orders');
+const Stores = require('../../models/stores');
 const Users = mongoose.model('users');
+const reponseError = require('../common/responseError');
+//const Orders = mongoose.model('orders');
+//const Stores = mongoose.model('stores');
 
 export let router = express.Router();
 
-router.use(function timeLog(req, res, next) {
+router.use('/order', function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
 
+/**
+ * GET /api/order
+ *
+ * @author seonhyungjo
+ * @summary 모든 주문내역 조회
+ * @private
+ * @memberof Admin
+ * @param
+ * @see None
+ * @returns «Query»
+ */
 router.get('/order', function(req, res) {
-    Orders.find(function(err, orderList) {
-        if (err) {
-            return res.status(500).send({ error: 'database failure' });
-        }
-        res.json(orderList);
-    });
+    Orders.find()
+        .then(orderList => {
+            if (!orderList) throw new Error('order not found');
+            res.status(200).send(orderList);
+        })
+        .catch(err => {
+            console.log(err);
+            reponseError(res, 'NOT_FIND_ODER');
+        });
 });
 
+/**
+ * GET /api/order/:branchCode
+ *
+ * @author seonhyungjo
+ * @summary 출고되지 않은 해당 지점 주문내역 조회
+ * @private
+ * @memberof Admin, User
+ * @param
+ * @see None
+ * @returns «Query»
+ */
+router.get('/order/:branchCode', function(req, res) {
+    Stores.find({ branchCode: req.params.branchCode })
+        .then(store => {
+            if (store.length == 0) {
+                throw new Error('Dont exit branchCode');
+            }
+
+            return Orders.findInCompleteOrder(req.params.branchCode);
+        })
+        .then(order => {
+            if (!order) throw new Error('order not found');
+            res.status(200).send(order);
+        })
+        .catch(err => {
+            console.log(err);
+            reponseError(res, 'NOT_FIND_ODER');
+        });
+});
+
+/**
+ * POST /api/order
+ *
+ * @author seonhyungjo
+ * @summary 주문내역 추가 또는 수정진행
+ * @private
+ * @memberof Admin
+ * @param
+ * @see None
+ * @returns «Query»
+ */
 router.post('/order', (req, res) => {
-    Orders.findOneByBranchCode(req.body.branchCode)
+    Orders.findInCompleteOrder(req.body.branchCode)
+        .then(order => {
+            if (order.length !== 0) {
+                //기존에 complete:false인 내역이 있을 경우 수정진행
+                console.log('Modified');
+                return Orders.findOneAndUpdateNew(req.body.branchCode, req.body);
+                //throw new Error('Already exit');
+            }
+            // 기존에 complete:false인 내역이 없을 경우 주문내역 추가
+            return Orders.create(req.body);
+        })
+        .then(() => {
+            res.status(200).send({ success: '0000' });
+        })
+        .catch(err => {
+            console.log(err);
+            reponseError(res, 'CREATE_ODER_ERROR');
+        });
+});
+
+/**
+ * PUT /api/order/complete/:branchCode
+ *
+ * @author seonhyungjo
+ * @summary 지점별 출고완료 처리하기
+ * @private
+ * @memberof Admin
+ * @param
+ * @see None
+ * @returns «Query»
+ */
+router.put('/order/complete/:branchCode', (req, res) => {
+    Orders.findInCompleteOrder(req.params.branchCode)
         .then(order => {
             if (order.length == 0) {
-                Orders.create(req.body)
-                    .then(result => res.send(result))
-                    .catch(err => res.status(500).send(err));
-            } else {
-                res.status(404).send({ err: 'already exit' });
+                console.log('Dont exit');
+                reponseError(res, 'DONT_EXIT');
             }
+            return Orders.changeCompleteTrue(req.params.branchCode);
         })
-        .catch(err => res.status(500).send(err));
-});
-
-router.get('/order/:branchCode', function(req, res) {
-    Users.find({ branchCode: req.params.branchCode })
-        .then(user => {
-            console.log(user);
-            if (user.length !== 0) {
-                Orders.findOneByBranchCode(req.params.branchCode)
-                    .then(order => {
-                        if (!order)
-                            return res
-                                .status(404)
-                                .send({ err: 'order not found' });
-                        res.json(order);
-                    })
-                    .catch(err => res.status(500).send(err));
-            } else {
-                res.status(500).send(err);
-            }
+        .then(() => {
+            res.status(200).send({ success: '0000' });
         })
-        .catch(err => res.status(500).send(err));
-});
-
-router.put('/order', (req, res) => {
-    Orders.findOneAndUpdateNew(req.body.branchCode, req.body)
-        .then(order => res.send(order))
-        .catch(err => res.status(500).send(err));
+        .catch(err => {
+            console.log(err);
+            reponseError(res, 'AREADY_COMPLETE');
+        });
 });
 
 router.delete('/order/:branchCode', (req, res) => {
