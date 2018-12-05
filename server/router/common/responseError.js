d38e0285f536a78ed27613dec36c2f2a3ccbbd54
SeonHyungJo <seonhyung.jo@gmail.com>
2018-12-05 23:17:04 +0900

    :house: seonhyungjo / fix / Error Status Code 변환

--- server/router/common/responseError.js
+++ server/router/common/responseError.js
@@ -1,49 +1,49 @@
 const reponseErrorType = (res, type) => {
     switch (type) {
         case 'LOGIN_FAIL':
-            res.status(500).send({ fail: '0001' });
+            res.status(200).send({ fail: '0001' });
             break;
         case 'REGISTER_FAIL':
-            res.status(500).send({ fail: '0002' });
+            res.status(200).send({ fail: '0002' });
             break;
         case 'NOT_LOGIN':
-            res.status(500).send({ fail: '1009' });
+            res.status(200).send({ fail: '1009' });
             break;
         //------------------------------------------------------
         case 'CREATE_USER_ERROR':
-            res.status(500).send({ fail: '1001' });
+            res.status(200).send({ fail: '1001' });
             break;
         case 'NOT_FIND_USER':
-            res.status(500).send({ fail: '1002' });
+            res.status(200).send({ fail: '1002' });
             break;
         case 'UPDATE_USER_ERROR':
-            res.status(500).send({ fail: '1003' });
+            res.status(200).send({ fail: '1003' });
             break;
         case 'DELETE_USER_ERROR':
-            res.status(500).send({ fail: '1004' });
+            res.status(200).send({ fail: '1004' });
             break;
         //------------------------------------------------------
         case 'CREATE_ODER_ERROR':
-            res.status(500).send({ fail: '3001' });
+            res.status(200).send({ fail: '3001' });
             break;
         case 'NOT_FIND_ODER':
-            res.status(500).send({ fail: '3002' });
+            res.status(200).send({ fail: '3002' });
             break;
         case 'UPDATE_ODER_ERROR':
-            res.status(500).send({ fail: '3003' });
+            res.status(200).send({ fail: '3003' });
             break;
         case 'DELETE_ODER_ERROR':
-            res.status(500).send({ fail: '3004' });
+            res.status(200).send({ fail: '3004' });
             break;
         case 'DONT_EXIT':
-            res.status(500).send({ fail: '3009' });
+            res.status(200).send({ fail: '3009' });
             break;
         case 'AREADY_COMPLETE':
-            res.status(500).send({ fail: '3010' });
+            res.status(200).send({ fail: '3010' });
             break;
         //------------------------------------------------------
         default:
-            res.status(500).send({ fail: '9999' });
+            res.status(200).send({ fail: '9999' });
             break;
     }
 };
