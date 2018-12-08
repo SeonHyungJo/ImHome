9c0d67203325abf294c142b0af2dc268fc493c8d
BKJang <jp302119@gmail.com>
2018-12-08 16:29:06 +0900

    :house: BKJang / fix / Warning 제거

--- front/src/component/product/StoreProduct.jsx
+++ front/src/component/product/StoreProduct.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import classNames from 'classnames';
+// import classNames from 'classnames';
 import { Button } from '../common';
 import { TableWithScroll } from '../table';
 
