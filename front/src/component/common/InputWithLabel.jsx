4daa47ce4a2b799ca786995f2b93e60bd5098ac5
BKJang <jp302119@gmail.com>
2018-12-08 16:27:29 +0900

    :house: BKJang / feat / 회원정보 수정 화면 redux 연동

--- front/src/component/common/InputWithLabel.jsx
+++ front/src/component/common/InputWithLabel.jsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 // 두개가 함께 있을땐 상단 (그 사이) 에 여백을 준다
 const Wrapper = styled.div`
     & + & {
-        margin-top: 1rem;
-        margin-bottom: 1rem;
+        margin-top: 0.8rem;
+        margin-bottom: 0.8rem;
     }
 `;
 
@@ -25,8 +25,8 @@ const Input = styled.input`
     border: 1px solid #c2c2c2;
     outline: none;
     border-radius: 3px;
-    height: 1.2em;
-    font-size: 1.2rem;
+    height: 2em;
+    font-size: 0.7rem;
     padding-left: 0.5rem;
     padding-right: 0.01rem;
     ::placeholder {
--- front/src/component/view/PopUserInfo.jsx
+++ front/src/component/view/PopUserInfo.jsx
@@ -9,6 +9,8 @@ import * as UserActions from '../../redux/modules/user';
 
 class PopUserInfo extends Component {
     render() {
+        const { branchName, name, cName, bNumber, bAddress, email, pNumber } = this.props.form.toJS();
+
         return (
             <PopUserWrapper style={{ display: this.props.displayPop ? 'block' : 'none' }}>
                 <PopUserTitle title="회원정보수정">
@@ -19,37 +21,44 @@ class PopUserInfo extends Component {
                         type="text"
                         name="custName"
                         placeholder="대표 성함"
+                        value={name}
                         autoFocus />
                     <InputWithLabel label="사업자 번호"
                         type="text"
                         name="custNumber"
                         placeholder="사업자 번호"
-                        autoFocus />
+                        value={bNumber}
+                    />
                     <InputWithLabel label="사업장 주소"
                         type="text"
                         name="custAddress"
                         placeholder="사업장 주소"
-                        autoFocus />
+                        value={bAddress}
+                    />
                     <InputWithLabel label="회사명"
                         type="text"
                         name="companyName"
                         placeholder="사업자 등록증 상의 회사명"
-                        autoFocus />
+                        value={cName}
+                    />
                     <InputWithLabel label="이메일 주소"
                         type="text"
                         name="custEmail"
                         placeholder="세금계산서 받으실 수 있는 이메일 주소"
-                        autoFocus />
+                        value={email}
+                    />
                     <InputWithLabel label="휴대폰 번호"
                         type="text"
                         name="custPhoneNumber"
                         placeholder="휴대폰 번호"
-                        autoFocus />
+                        value={pNumber}
+                    />
                     <InputWithLabel label="지점명"
                         type="text"
                         name="storName"
                         placeholder="지점명"
-                        autoFocus />
+                        value={branchName}
+                    />
                 </PopUserContent>
                 <Button style={{ marginRight: '1rem', width: '6rem' }}>수정하기</Button>
                 <Button style={{ width: '6rem' }} onClick={this.props.closePop}>닫기</Button>
