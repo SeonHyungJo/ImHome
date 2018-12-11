de0dafbf53c0383a4b5971c77a44246ec71c5498
eugene94 <eugenekang94@gmail.com>
2018-12-11 18:02:13 +0900

    :house: jinseong / feat / server model 생성( company )

--- server/router/api/router.js
+++ server/router/api/router.js
@@ -5,11 +5,13 @@ import { router as user } from './user';
 import { router as product } from './product';
 import { router as order } from './order';
 import { router as store } from './store';
+import { router as company } from './company';
 
 routers.push(user);
 routers.push(product);
 routers.push(order);
 routers.push(store);
+routers.push(company);
 
 // 추후 이런방식으로 가고싶은데 에러남 => Schema hasn't been registered for model "users"가 order.js에서 에러
 // fs.readdirSync(__dirname)
