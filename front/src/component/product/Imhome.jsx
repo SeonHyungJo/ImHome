52ea8a4aebdb745de72f802dd4ec77132e9d08d3
eugene94 <eugenekang94@gmail.com>
2018-12-14 22:28:15 +0900

    :house: jinseong / style / Imhome 컴포넌트 CSS 변경

--- front/src/component/product/Imhome.jsx
+++ front/src/component/product/Imhome.jsx
@@ -14,17 +14,39 @@ const ContentWrapper = styled.div`
     background-color: white;
     padding: 10px;
 
-    .footerContainer {
-        padding: 0px 0px 10px 0px;
-        width: 100%;
-        display: flex;
-        // border: solid 1px black;
-    }
-
     .category {
+        display: flex;
+        justify-content: space-between;
         margin: 10px 10px 0px 0px;
         padding: 0px 0px 0px 0px;
-        // border: solid 1px black;
+        height: 6rem;
+        .categoryMain {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width: 70%;
+
+            .name {
+                font-weight: bold;
+            }
+            .desc {
+                color: #808080;
+            }
+        }
+        .categorySub {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 30%;
+            font-size: 50px;
+        }
+        .categorySubButton {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            width: 30%;
+        }
     }
 
     .clicked {
@@ -32,61 +54,31 @@ const ContentWrapper = styled.div`
         color: #ffffff;
     }
 
-    .categoryDesc {
+    .footerContainer {
+        padding: 0px 0px 10px 0px;
+        width: 100%;
         display: flex;
-        justify-content: space-between;
         // border: solid 1px black;
-        height: 100px; // 수정필요
-    }
-
-    .categoryMain {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        width: 70%;
-        // border: solid 1px black;
-    }
-
-    .categoryMain > .name {
-        font-weight: bold;
-    }
-
-    .categoryMain > .desc {
-        color: #808080;
-    }
-
-    .categorySub {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 30%;
-        font-size: 50px;
-        // border: solid 1px black;
-    }
-
-    .categorySubButton {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        width: 30%;
     }
 `;
 
 const MainContainer = styled.div`
     padding: 10px 0px 0px 10px;
-    width: 40vw;
+    width: 35vw;
     height: 75vh;
     overflow: auto;
-    // border: solid 1px black;
 `;
 
 const ProductFormContainer = styled.div`
-    width: 40vw;
+    width: 45vw;
     height: 75vh;
     background-color: white;
     padding: 10px;
-    overflow: auto;z
+    overflow: auto;
+    .tableTitle {
+        font-size: 1.3rem;
+        margin-bottom: 1rem;
+    }
 `;
 
 const Label = styled.div`
@@ -107,8 +99,6 @@ const Input = styled.input`
     border-radius: 3px;
     height: 2em;
     font-size: 0.7rem;
-    // padding-left: 0.5rem;
-    // padding-right: 0.01rem;
     ::placeholder {
         font-size: 0.7rem;
         color: #7d7d7d;
@@ -117,8 +107,6 @@ const Input = styled.input`
 
 const SmallButton = styled.button`
     margin-top: 0.5rem;
-    // padding-top: 0.5rem;
-    // padding-bottom: 0.5rem;
     border: 2px solid #fe4c8d;
     border-radius: 3px;
     background: white;
@@ -129,7 +117,20 @@ const SmallButton = styled.button`
     width: 3rem;
     height: 2rem;
     cursor: pointer;
-    // font-weight: bold;
+`;
+
+const TableButton = styled.button`
+    // margin-top: 0.5rem;
+    border: 2px solid #c2c2c2;
+    border-radius: 3px;
+    background: white;
+    color: #fe4c8d;
+
+    text-align: center;
+    font-size: 0.6rem;
+    width: 3rem;
+    height: 2rem;
+    cursor: pointer;
 `;
 
 const Button = styled.button`
@@ -150,12 +151,53 @@ const Button = styled.button`
     font-weight: bold;
 `;
 
+const Table = styled.table`
+    border-top: 2px solid #fe4c8d;
+    width: 100%;
+    border-collapse: collapse;
+    font-size: 1rem;
+    color: #333;
+    table-layout: fixed;
+    border-spacing: 0;
+    th {
+        text-align: center;
+        border-right: 1px solid #e0e0e0;
+        border-bottom: 1px solid #e0e0e0;
+        background: #fafafa;
+        height: 35px;
+        line-height: 18px;
+        color: #666666;
+        font-size: 1rem;
+        padding: 5px;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    td {
+        border-right: 1px solid #e0e0e0;
+        border-bottom: 1px solid #e0e0e0;
+        padding: 5px;
+        line-height: 22px;
+        color: #666666;
+        font-size: 0.9rem;
+        height: 30px;
+        box-sizing: border-box;
+        letter-spacing: -0.04em;
+    }
+    .checkboxTd {
+        width: 7%;
+    }
+    .tableAlignCenter {
+        text-align: center;
+    }
+`;
 class Imhome extends Component {
     constructor(props) {
         super(props);
         this.state = {
             clickedCate: { index: -1, _id: -1, itemName: '-' },
             newCategory: { state: false, newName: '', newDesc: '' },
+            newItem: { state: false, newName: '', newVolume: '', newCost: '' },
             categories: props.categories
         };
     }
@@ -247,10 +289,12 @@ class Imhome extends Component {
     render() {
         const items = this.props.product.items;
         const { clickedCate, newCategory } = this.state;
+        const detailItem = items.filter(item => item.parentId === clickedCate._id);
+        const defaultTable = [0, 1, 2, 3, 4];
         return (
             <ContentWrapper>
                 <MainContainer>
-                    <div className={'productComponent'} id={'productComponent'}>
+                    <div className={'productComponent'}>
                         {!!this.props.categories.length > 0 ? (
                             this.props.categories.map((item, index) => (
                                 <div
@@ -261,21 +305,19 @@ class Imhome extends Component {
                                     }
                                     key={index}
                                 >
-                                    <div className={'categoryDesc'}>
-                                        <div className={'categoryMain'}>
-                                            <div className={'name'}>{item.itemName}</div>
-                                            <div className={'desc'}>{item.itemDesc}</div>
-                                        </div>
-                                        <div
-                                            className={'categorySub'}
-                                            onClick={() =>
-                                                this._clickCategory(index, item._id, item.itemName)
-                                            }
-                                        >
-                                            <span>
-                                                {this.state.clickedCate.index === index ? '>' : '<'}
-                                            </span>
-                                        </div>
+                                    <div className={'categoryMain'}>
+                                        <div className={'name'}>{item.itemName}</div>
+                                        <div className={'desc'}>{item.itemDesc}</div>
+                                    </div>
+                                    <div
+                                        className={'categorySub'}
+                                        onClick={() =>
+                                            this._clickCategory(index, item._id, item.itemName)
+                                        }
+                                    >
+                                        <span>
+                                            {this.state.clickedCate.index === index ? '>' : '<'}
+                                        </span>
                                     </div>
                                 </div>
                             ))
@@ -284,38 +326,36 @@ class Imhome extends Component {
                         )}
                         {newCategory.state === true ? (
                             <div className={classNames('category')}>
-                                <div className={'categoryDesc'}>
-                                    <div className={'categoryMain'}>
-                                        <div className={'name'}>
-                                            <Label>이름</Label>
-                                            <Input
-                                                type="text"
-                                                name="newName"
-                                                placeholder="이름"
-                                                onChange={this._handleChange}
-                                                value={newCategory.newName}
-                                            />
-                                        </div>
-                                        <div className={'desc'}>
-                                            <Label>설명</Label>
-                                            <Input
-                                                type="text"
-                                                name="newDesc"
-                                                placeholder="설명"
-                                                onChange={this._handleChange}
-                                                value={newCategory.newDesc}
-                                            />
-                                        </div>
+                                <div className={'categoryMain'}>
+                                    <div className={'name'}>
+                                        <Label>이름</Label>
+                                        <Input
+                                            type="text"
+                                            name="newName"
+                                            placeholder="이름"
+                                            onChange={this._handleChange}
+                                            value={newCategory.newName}
+                                        />
                                     </div>
-                                    <div className={'categorySubButton'}>
-                                        <SmallButton onClick={() => this._createCategory()}>
-                                            확인
-                                        </SmallButton>
-                                        <SmallButton onClick={() => this._cancelCategory()}>
-                                            취소
-                                        </SmallButton>
-                                        {/* <button onClick={() => this._createCategory()}></button> */}
+                                    <div className={'desc'}>
+                                        <Label>설명</Label>
+                                        <Input
+                                            type="text"
+                                            name="newDesc"
+                                            placeholder="설명"
+                                            onChange={this._handleChange}
+                                            value={newCategory.newDesc}
+                                        />
                                     </div>
+                                </div>
+                                <div className={'categorySubButton'}>
+                                    <SmallButton onClick={() => this._createCategory()}>
+                                        확인
+                                    </SmallButton>
+                                    <SmallButton onClick={() => this._cancelCategory()}>
+                                        취소
+                                    </SmallButton>
+                                    {/* <button onClick={() => this._createCategory()}></button> */}
                                 </div>
                             </div>
                         ) : (
@@ -333,25 +373,54 @@ class Imhome extends Component {
                     )}
                 </MainContainer>
                 <ProductFormContainer>
-                    {/* <TableWithScroll
-                        headerData={this.state.headerData}
-                        data={this.state.data}
-                        gridTitle={this.state.tableTitle + ' 상세'}
-                    /> */}
-                    <div>{this.state.clickedCate.itemName}</div>
+                    <div className={'tableTitle'}>{this.state.clickedCate.itemName}</div>
                     <div>
-                        {/* {this.props.product.items.filter(item => item.parentId===this.state)} */}
-                        {items
-                            .filter(item => item.parentId === clickedCate._id)
-                            .map(child => (
-                                <div key={child._id}>
-                                    {child.itemName +
-                                        ' | ' +
-                                        child.itemVolume +
-                                        ' | ' +
-                                        child.itemCost}
-                                </div>
-                            ))}
+                        <Table>
+                            <tbody>
+                                <tr>
+                                    <th className={classNames('checkboxTd', 'tableAlignCenter')} />
+                                    <th>품목</th>
+                                    <th>단위</th>
+                                    <th>가격</th>
+                                    <th>수정</th>
+                                </tr>
+                                {!!detailItem.length > 0
+                                    ? detailItem.map(child => (
+                                          <tr key={child._id}>
+                                              <td
+                                                  className={classNames(
+                                                      'checkboxTd',
+                                                      'tableAlignCenter'
+                                                  )}
+                                              >
+                                                  <input type="checkbox" id={child._id} />
+                                              </td>
+                                              <td>{child.itemName}</td>
+                                              <td>{child.itemVolume}</td>
+                                              <td>{child.itemCost}</td>
+                                              <td className={classNames('tableAlignCenter')}>
+                                                  <TableButton>수정</TableButton>
+                                              </td>
+                                          </tr>
+                                      ))
+                                    : defaultTable.map((i, index) => (
+                                          <tr key={index}>
+                                              <td
+                                                  className={classNames(
+                                                      'checkboxTd',
+                                                      'tableAlignCenter'
+                                                  )}
+                                              >
+                                                  <input type="checkbox" id={i} />
+                                              </td>
+                                              <td>-</td>
+                                              <td>-</td>
+                                              <td>-</td>
+                                              <td>-</td>
+                                          </tr>
+                                      ))}
+                            </tbody>
+                        </Table>
                     </div>
                     <hr />
                     <div className={'footerContainer'}>
