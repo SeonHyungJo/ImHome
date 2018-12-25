1632023ccec41716b20af0472f9d4b309cedc560
BKJang <jp302119@gmail.com>
2018-12-25 21:36:23 +0900

    :house: BKJang / fix / App.jsx 원복

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -15,48 +15,48 @@ import * as AuthActions from './redux/modules/auth';
 
 class App extends Component {
 
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         isLogin: true
-    //     };
-    // }
+    constructor() {
+        super();
+        this.state = {
+            isLogin: true
+        };
+    }
 
-    // initializeUserInfo = async () => {
-    //     const loggedInfo = localStorage.getItem('accessToken'); // 로그인 정보를 로컬스토리지에서 가져옵니다.
-    //     if (!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
+    initializeUserInfo = async () => {
+        const loggedInfo = localStorage.getItem('accessToken'); // 로그인 정보를 로컬스토리지에서 가져옵니다.
+        if (!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
 
-    //     const { AuthActions } = this.props;
+        const { AuthActions } = this.props;
 
-    //     try {
-    //         await AuthActions.checkStatus();
-    //         const loggedInfo = this.props.result.toJS();
+        try {
+            await AuthActions.checkStatus();
+            const loggedInfo = this.props.result.toJS();
 
-    //         if (loggedInfo.success && loggedInfo.success !== '0000') {
-    //             localStorage.removeItem('accessToken');
-    //             this.setState({ isLogin: false });
-    //         }
-    //     } catch (e) {
-    //         localStorage.removeItem('accessToken');
-    //         this.setState({ isLogin: false });
-    //     }
-    // }
+            if (loggedInfo.success && loggedInfo.success !== '0000') {
+                localStorage.removeItem('accessToken');
+                this.setState({ isLogin: false });
+            }
+        } catch (e) {
+            localStorage.removeItem('accessToken');
+            this.setState({ isLogin: false });
+        }
+    }
 
-    // componentDidMount() {
-    //     this.initializeUserInfo();
-    // }
+    componentDidMount() {
+        this.initializeUserInfo();
+    }
 
     render() {
         const PATH = '/admin';
         const { store } = this.props;
 
-        // if (!this.state.isLogin) {
-        //     return (
-        //         <BrowserRouter>
-        //             <Redirect to="/login" push />
-        //         </BrowserRouter>
-        //     );
-        // }
+        if (!this.state.isLogin) {
+            return (
+                <BrowserRouter>
+                    <Redirect to="/login" push />
+                </BrowserRouter>
+            );
+        }
         return (
             <Provider store={store}>
                 <div>
