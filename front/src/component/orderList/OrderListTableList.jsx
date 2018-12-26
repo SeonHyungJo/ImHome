cfaccd8c412ab10e6c7df3625d790506f0df4dca
SeonHyungJo <seonhyung.jo@gmail.com>
2018-12-27 00:18:16 +0900

    :house: SeonHyungJo / feat / 주문내역 overflow scroll 처리 진행

--- front/src/component/orderList/OrderListTableList.jsx
+++ front/src/component/orderList/OrderListTableList.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import classNames from 'classnames';
+import styled from 'styled-components';
+
+const TableList = styled.div`
+    overflow: auto;
+
+    /* Scroll Custom */
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-button {
+        width: 0px;
+        height: 0px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: white;
+        border-radius: 0px;
+    }
+`;
 
 const OrderListTableList = ({ orderList }) => (
-    <div className={classNames('tbody', 'underLineDash')}>
+    <TableList className={classNames('tbody', 'underLineDash')}>
         {orderList instanceof Array &&
             orderList.map(order => (
                 <div key={order.itemCode} className={'tbodyTr'}>
@@ -11,7 +31,7 @@ const OrderListTableList = ({ orderList }) => (
                     <div className={classNames('tbodyTd')}>{`: $ ${order.itemCost}`}</div>
                 </div>
             ))}
-    </div>
+    </TableList>
 );
 
 export default OrderListTableList;
