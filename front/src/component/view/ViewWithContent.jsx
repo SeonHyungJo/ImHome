be3d87a2d3771772f4d0f9bcf8d66be0c9f6949b
SeonHyungJo <seonhyung.jo@gmail.com>
2019-01-29 22:48:55 +0900

    :house: SeonHyungJo / fix / 전체 formatting 진행

--- front/src/component/view/ViewWithContent.jsx
+++ front/src/component/view/ViewWithContent.jsx
@@ -8,47 +8,51 @@ const ViewContentWrapper = styled.div`
 `;
 
 const Table = styled.table`
-    border-top: 2px solid #fe4c8d;
-    border-bottom: 2px solid #fe4c8d;
-    width: 100%;
-    border-collapse: collapse;
-    font-size: 12px;
-    color: #333;
-    table-layout: fixed;
-    border-spacing: 0;
-    border-right : none;
-    th {
-        text-align: left;
-        width: 9rem
-        border-right: 1px solid #e0e0e0;
-        border-bottom: 1px solid #e0e0e0;
-        background: #fafafa;
-        height: 3rem;
-        line-height: 18px;
-        color: #666666;
-        font-size: 13px;
-        font-weight: bold;
-        padding: 5px;
-        padding-left: 1rem;
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-        text-overflow:ellipsis; overflow:hidden; white-space:nowrap;
-    };
-    td {
-        border-right: 1px solid #e0e0e0;
-        border-bottom: 1px solid #e0e0e0;
-        padding: 5px;
-        padding-left: 1rem;
-        line-height: 22px;
-        color: #666666;
-        font-size: 0.8rem;
-        font-weight: bold;
-        height: 3rem;
-        box-sizing: border-box;
-        letter-spacing: -0.04em;
-        text-overflow:ellipsis; overflow:hidden; white-space:nowrap;
-    };
+  border-top: 2px solid #fe4c8d;
+  border-bottom: 2px solid #fe4c8d;
+  width: 100%;
+  border-collapse: collapse;
+  font-size: 12px;
+  color: #333;
+  table-layout: fixed;
+  border-spacing: 0;
+  border-right: none;
+  th {
+    text-align: left;
+    width: 9rem;
+    border-right: 1px solid #e0e0e0;
+    border-bottom: 1px solid #e0e0e0;
+    background: #fafafa;
+    height: 3rem;
+    line-height: 18px;
+    color: #666666;
+    font-size: 13px;
+    font-weight: bold;
+    padding: 5px;
+    padding-left: 1rem;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  td {
+    border-right: 1px solid #e0e0e0;
+    border-bottom: 1px solid #e0e0e0;
+    padding: 5px;
+    padding-left: 1rem;
+    line-height: 22px;
+    color: #666666;
+    font-size: 0.8rem;
+    font-weight: bold;
+    height: 3rem;
+    box-sizing: border-box;
+    letter-spacing: -0.04em;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
 `;
 
 const ViewWithContent = ({ title, children }) => (
