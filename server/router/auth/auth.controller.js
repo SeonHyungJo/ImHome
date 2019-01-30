310d06d22160f58f1627b5b9d100a155e9238733
SeonHyungJo <seonhyung.jo@gmail.com>
2019-01-31 00:15:58 +0900

    :house: SeonHyungJo / feat / 회원가입 로직 변경 및 에러코드 정리

--- server/router/auth/auth.controller.js
+++ server/router/auth/auth.controller.js
@@ -1,9 +1,9 @@
-const Users = require('../../models/users');
-const Store = require('../../models/stores');
-const crypto = require('../common/cryptoModule');
-const reponseError = require('../common/responseError');
-const reponseSuccess = require('../common/responseSuccess');
-const jwt = require('jsonwebtoken');
+import Users from '../../models/users';
+import Store from '../../models/stores';
+import crypto from '../common/cryptoModule';
+import reponseError from '../common/responseError';
+import reponseSuccess from '../common/responseSuccess';
+import jwt from 'jsonwebtoken';
 
 /**
  * POST /api/register
@@ -18,74 +18,104 @@ const jwt = require('jsonwebtoken');
  *     name : NAME,
  *     bNumber : BUSINESS_NUMBER,
  *     bAddress : BUSINESS_ADDRESS,
+ *     bPhoneNumber : PHONE_NUMBER,
  *     cName : COMPANY_NAME,
  *     email : EMAIL,
- *     bPhoneNumber : PHONE_NUMBER,
+ *     pNumber : ???
  *     branchName : BRANCH_NAME,
  *     branchCode : BRANCH_CODE
  * }
- * 190123 수정 - 비밀번호 암호화 추가 - @author BKJang
+ * 190123 수정 - 비밀번호 암호화 추가 - @update BKJang
+ * 190130 수정 - 코드 리팩토링 - @update SeonHyungJo
  */
 
 exports.register = (req, res) => {
-    let userInfo = req.body;
-    /*
-     * 비밀번호 암호화
-     */
-    userInfo = crypto.cryptoUserInfo(userInfo);
+  const userInfo = req.body;
 
-    /*
-        아이디가 존재한다면 ERROR처리 
-        없다면 새로 생성
-    */
-    const checkId = user => {
-        const userId = userInfo.id;
-        return new Promise((resolve, reject) => {
-            let result = true;
-            if (user && userId === user.id) result = false;
+  // 빈값 체크
+  const blankCheck = insertUserInfo => {
+    return new Promise((resolve, reject) => {
+      Object.keys(insertUserInfo).map(key => {
+        if (!insertUserInfo[key])
+          reject(new Error(`BLANK_${key.toUpperCase()}`));
+      });
 
-            return resolve(result);
-        });
-    };
+      resolve(insertUserInfo);
+    });
+  };
 
-    const create = checkIdResult => {
-        const branchCode = userInfo.branchCode;
+  // 비밀번호 암호화
+  const cryptoUserInfo = insertUserInfo => {
+    return new Promise(resolve => {
+      const cryptoUserInfo = {
+        ...insertUserInfo,
+        ...crypto.cryptoUserInfo(userInfo)
+      };
 
-        return new Promise((resolve, reject) => {
-            if (!checkIdResult) {
-                return resolve('REGISTER_FAIL_ID');
-            }
-            Store.checkBranch(branchCode).then(result => {
-                if (result != null) {
-                    userInfo.branchName = result.branchName;
-                    return resolve(Users.create(userInfo));
-                }
+      resolve(cryptoUserInfo);
+    });
+  };
 
-                return reject(new Error('Not Find Branch'));
-            });
-        });
-    };
+  // 아이디 중복확인
+  const checkUserId = cryptoInfo => {
+    return new Promise((resolve, reject) => {
+      Users.findOneByUserId(cryptoInfo.id)
+        .then(foundUserInfo => {
+          const result =
+            foundUserInfo && cryptoInfo.id === foundUserInfo.id ? false : true;
 
-    const respond = resultKey => {
-        if (resultKey === 'REGISTER_FAIL_ID') {
-            reponseError(res, resultKey);
-            return;
-        }
-        reponseSuccess(res);
-    };
+          result ? resolve(cryptoInfo) : reject(new Error('ALREADY_EXIST_ID'));
+        })
+        .catch(new Error('ERROR_FIND_USERID'));
+    });
+  };
 
-    // run when there is an error (username exists)
-    const onError = error => {
-        console.log(error.message);
-        reponseError(res, 'REGISTER_FAIL');
-    };
+  // BranchName 확인
+  const checkBranchCode = cryptoInfo => {
+    return new Promise((resolve, reject) => {
+      Store.checkBranch(cryptoInfo.branchCode)
+        .then(foundBranch => {
+          if (foundBranch) {
+            cryptoInfo = {
+              ...cryptoInfo,
+              branchName: foundBranch.branchName,
+              checkUser: true
+            };
 
-    // 해당 아이디 복수 확인
-    Users.findOneByUserId(userInfo.id)
-        .then(checkId)
-        .then(create)
-        .then(respond)
-        .catch(onError);
+            resolve(cryptoInfo);
+          }
+
+          reject(new Error('BLANK_BRANCHNAME'));
+        })
+        .catch(new Error('ERROR_CHECK_BRANCH'));
+    });
+  };
+
+  const create = cryptoInfo => {
+    return new Promise(() => {
+      Users.create(cryptoInfo)
+        .then(createResult => {
+          console.log(createResult);
+          reponseSuccess(res);
+        })
+        .catch(new Error('ERROR_CREATE'));
+    });
+  };
+
+  // run when there is an error (username exists)
+  const onError = error => {
+    console.log(error.message);
+    //reponseError(res, 'REGISTER_FAIL');
+    reponseError(res, error.message);
+  };
+
+  // 해당 아이디 복수 확인
+  blankCheck(userInfo)
+    .then(cryptoUserInfo)
+    .then(checkUserId)
+    .then(checkBranchCode)
+    .then(create)
+    .catch(onError);
 };
 
 /**
@@ -101,61 +131,59 @@ exports.register = (req, res) => {
  * }
  */
 exports.login = (req, res) => {
-    const { id, password } = req.body;
-    console.log(id);
-    let userInfo = req.body;
-    const secret = req.app.get('jwt-secret');
-    let checkAdmin = false;
+  const { id, password } = req.body;
+  console.log(id);
+  const secret = req.app.get('jwt-secret');
+  let checkAdmin = false;
 
-    const check = user => {
-        const checkPassword = user.verify(password);
-        checkAdmin = user.checkAdmin;
-        console.log(user);
-        if (checkPassword) {
-            return new Promise((resolve, reject) => {
-                jwt.sign(
-                    {
-                        id: user.id,
-                        username: user.username,
-                        branchCode: user.branchCode,
-                        admin: checkAdmin
-                    },
-                    secret,
-                    {
-                        expiresIn: '30m',
-                        issuer: 'imhome.com',
-                        subject: 'userInfo',
-                        algorithm: 'HS512'
-                    },
-                    (err, token) => {
-                        if (err) reject(err);
-                        resolve(token);
-                    }
-                );
-            });
-        } else {
-            throw new Error('login failed');
-        }
-    };
+  const check = user => {
+    const checkPassword = user.verify(password);
+    checkAdmin = user.checkAdmin;
+    if (checkPassword) {
+      return new Promise((resolve, reject) => {
+        jwt.sign(
+          {
+            id: user.id,
+            username: user.username,
+            branchCode: user.branchCode,
+            admin: checkAdmin
+          },
+          secret,
+          {
+            expiresIn: '30m',
+            issuer: 'imhome.com',
+            subject: 'userInfo',
+            algorithm: 'HS512'
+          },
+          (err, token) => {
+            if (err) reject(err);
+            resolve(token);
+          }
+        );
+      });
+    } else {
+      throw new Error('login failed');
+    }
+  };
 
-    const respond = token => {
-        console.log(token);
-        res.send({
-            success: '0000',
-            imhomeToken: token,
-            checkAdmin: checkAdmin
-        });
-    };
+  const respond = token => {
+    console.log(token);
+    res.send({
+      success: '0000',
+      imhomeToken: token,
+      checkAdmin: checkAdmin
+    });
+  };
 
-    const onError = error => {
-        console.log(error);
-        reponseError(res, 'LOGIN_FAIL');
-    };
+  const onError = error => {
+    console.log(error);
+    reponseError(res, 'LOGIN_FAIL');
+  };
 
-    Users.findOneByUserId(id)
-        .then(check)
-        .then(respond)
-        .catch(onError);
+  Users.findOneByUserId(id)
+    .then(check)
+    .then(respond)
+    .catch(onError);
 };
 
 /*
@@ -176,31 +204,31 @@ exports.login = (req, res) => {
  */
 
 exports.check = (req, res) => {
-    const token = req.headers['x-access-token'] || req.query.token;
+  const token = req.headers['x-access-token'] || req.query.token;
 
-    if (!token) {
-        console.log('Dont have Token');
-        reponseError(res, 'NOT_LOGIN');
-    }
+  if (!token) {
+    console.log('Dont have Token');
+    reponseError(res, 'NOT_LOGIN');
+  }
 
-    const checkToken = new Promise((resolve, reject) => {
-        jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
-            if (err) reject(err);
-            resolve(decoded);
-        });
+  const checkToken = new Promise((resolve, reject) => {
+    jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+      if (err) reject(err);
+      resolve(decoded);
     });
+  });
 
-    const respond = token => {
-        res.json({
-            success: '0000',
-            info: token
-        });
-    };
+  const respond = token => {
+    res.json({
+      success: '0000',
+      info: token
+    });
+  };
 
-    const onError = error => {
-        console.log(error.message);
-        reponseError(res, 'NOT_LOGIN');
-    };
+  const onError = error => {
+    console.log(error.message);
+    reponseError(res, 'NOT_LOGIN');
+  };
 
-    checkToken.then(respond).catch(onError);
+  checkToken.then(respond).catch(onError);
 };
--- server/router/common/responseError.js
+++ server/router/common/responseError.js
@@ -1,66 +1,112 @@
 const reponseErrorType = (res, type) => {
-    switch (type) {
-        case 'LOGIN_FAIL':
-            res.status(200).send({ fail: '0001' });
-            break;
-        case 'REGISTER_FAIL':
-            res.status(200).send({ fail: '0002' });
-            break;
-        case 'REGISTER_FAIL_ID':
-            res.status(200).send({ fail: '0003' });
-            break;
-        case 'NOT_LOGIN':
-            res.status(200).send({ fail: '1009' });
-            break;
-        //------------------------------------------------------
-        case 'CREATE_USER_ERROR':
-            res.status(200).send({ fail: '1001' });
-            break;
-        case 'NOT_FIND_USER':
-            res.status(200).send({ fail: '1002' });
-            break;
-        case 'UPDATE_USER_ERROR':
-            res.status(200).send({ fail: '1003' });
-            break;
-        case 'DELETE_USER_ERROR':
-            res.status(200).send({ fail: '1004' });
-            break;
-        //------------------------------------------------------
-        case 'CREATE_ODER_ERROR':
-            res.status(200).send({ fail: '3001' });
-            break;
-        case 'NOT_FIND_ODER':
-            res.status(200).send({ fail: '3002' });
-            break;
-        case 'UPDATE_ODER_ERROR':
-            res.status(200).send({ fail: '3003' });
-            break;
-        case 'DELETE_ODER_ERROR':
-            res.status(200).send({ fail: '3004' });
-            break;
-        case 'NOT_FIND_BRANCH':
-            res.status(200).send({ fail: '3004' });
-            break;
-        case 'DONT_EXIT':
-            res.status(200).send({ fail: '3009' });
-            break;
-        case 'AREADY_COMPLETE':
-            res.status(200).send({ fail: '3010' });
-            break;
-        case 'CANT_FIND_PRODUCT':
-            res.status(200).send({ fail: '3011' });
-            break;
-        case 'CREATE_PRODUCT_ERROR':
-            res.status(200).send({ fail: '3012' });
-            break;
-        case 'UPDATE_ITEM_ERROR':
-            res.status(200).send({ fail: '3013' });
-            break;
-        //------------------------------------------------------
-        default:
-            res.status(200).send({ fail: '9999' });
-            break;
-    }
+  switch (type) {
+    case 'LOGIN_FAIL':
+      res.status(200).send({ fail: '0001' });
+      break;
+    //------------------------------------------------------
+
+    /**
+     * 회원가입관련 에러코드
+     */
+    case 'REGISTER_FAIL':
+      res.status(200).send({ fail: '0201' });
+      break;
+    case 'BLANK_ID':
+      res.status(200).send({ fail: '0210' });
+      break;
+    case 'BLANK_PASSWORD':
+      res.status(200).send({ fail: '0211' });
+      break;
+    case 'BLANK_NAME':
+      res.status(200).send({ fail: '0212' });
+      break;
+    case 'BLANK_BNUMBER':
+      res.status(200).send({ fail: '0213' });
+      break;
+    case 'BLANK_BADDRESS':
+      res.status(200).send({ fail: '0214' });
+      break;
+    case 'BLANK_CNAME':
+      res.status(200).send({ fail: '0215' });
+      break;
+    case 'BLANK_EMAIL':
+      res.status(200).send({ fail: '0216' });
+      break;
+    case 'BLANK_BPHONENUMBER':
+      res.status(200).send({ fail: '0217' });
+      break;
+    case 'BLANK_BRANCHCODE':
+      res.status(200).send({ fail: '0218' });
+      break;
+    case 'BLANK_BRANCHNAME':
+      res.status(200).send({ fail: '0219' });
+      break;
+    case 'ALREADY_EXIST_ID':
+      res.status(200).send({ fail: '0300' });
+      break;
+    case 'ERROR_FIND_USERID':
+      res.status(200).send({ fail: '0400' });
+      break;
+    case 'ERROR_CHECK_BRANCH':
+      res.status(200).send({ fail: '0408' });
+      break;
+    case 'ERROR_CREATE':
+      res.status(200).send({ fail: '0500' });
+      break;
+
+    //------------------------------------------------------
+    case 'NOT_LOGIN':
+      res.status(200).send({ fail: '1009' });
+      break;
+    //------------------------------------------------------
+    case 'CREATE_USER_ERROR':
+      res.status(200).send({ fail: '1001' });
+      break;
+    case 'NOT_FIND_USER':
+      res.status(200).send({ fail: '1002' });
+      break;
+    case 'UPDATE_USER_ERROR':
+      res.status(200).send({ fail: '1003' });
+      break;
+    case 'DELETE_USER_ERROR':
+      res.status(200).send({ fail: '1004' });
+      break;
+    //------------------------------------------------------
+    case 'CREATE_ODER_ERROR':
+      res.status(200).send({ fail: '3001' });
+      break;
+    case 'NOT_FIND_ODER':
+      res.status(200).send({ fail: '3002' });
+      break;
+    case 'UPDATE_ODER_ERROR':
+      res.status(200).send({ fail: '3003' });
+      break;
+    case 'DELETE_ODER_ERROR':
+      res.status(200).send({ fail: '3004' });
+      break;
+    case 'NOT_FIND_BRANCH':
+      res.status(200).send({ fail: '3004' });
+      break;
+    case 'DONT_EXIT':
+      res.status(200).send({ fail: '3009' });
+      break;
+    case 'AREADY_COMPLETE':
+      res.status(200).send({ fail: '3010' });
+      break;
+    case 'CANT_FIND_PRODUCT':
+      res.status(200).send({ fail: '3011' });
+      break;
+    case 'CREATE_PRODUCT_ERROR':
+      res.status(200).send({ fail: '3012' });
+      break;
+    case 'UPDATE_ITEM_ERROR':
+      res.status(200).send({ fail: '3013' });
+      break;
+    //------------------------------------------------------
+    default:
+      res.status(200).send({ fail: '9999' });
+      break;
+  }
 };
 
 module.exports = reponseErrorType;
