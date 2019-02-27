d48ec3066c3dd6b060065b8cd6410dd9b3f9b864
SeonHyungJo <seonhyung.jo@gmail.com>
2019-02-27 22:16:31 +0900

    :house: SeonHyungJo / fix / 출고내역 내림차순 & api수정

--- front/src/lib/api/releaseList.js
+++ front/src/lib/api/releaseList.js
@@ -2,10 +2,7 @@ import * as ApiService from './ApiService';
 
 export const getStoreList = () => ApiService.get('/api/store/list/');
 export const getOrderList = (storeId, startDate, endDate) => (startDate && endDate
-  ? ApiService.get(
-    '/api/order/list/',
-    `${storeId}?startDate=${startDate.toString()}&endDate=${endDate.toString()}`,
-  )
+  ? ApiService.get('/api/order/list/', `${storeId}?startDate=${startDate}&endDate=${endDate}`)
   : ApiService.get('/api/order/list/', storeId));
 export const getOrderData = custNo => ApiService.get('/api/order/', custNo);
 export const deleteOrderData = custNo => ApiService.del('/api/order/', custNo);
