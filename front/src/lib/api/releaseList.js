b727dc407ecda8ac14188a3cb9fd88b3e119c3cc
SeonHyungJo <seonhyung.jo@gmail.com>
2019-03-03 02:01:37 +0900

    :house: SeonHyungJo / fix / releaseList 수정

--- front/src/lib/api/releaseList.js
+++ front/src/lib/api/releaseList.js
@@ -1,14 +1,15 @@
 import * as ApiService from './ApiService';
 
 export const getStoreList = () => ApiService.get('/api/store/list/');
+
 export const getOrderList = (storeId, startDate, endDate) => (startDate && endDate
   ? ApiService.get('/api/order/list/', `${storeId}?startDate=${startDate}&endDate=${endDate}`)
   : ApiService.get('/api/order/list/', storeId));
+
 export const getOrderData = custNo => ApiService.get('/api/order/', custNo);
+
 export const deleteOrderData = custNo => ApiService.del('/api/order/', custNo);
+
 export const downloadExcel = (storeId, startDate, endDate) => (startDate && endDate
-  ? ApiService.post(
-    '/api/order/excel/',
-    { storeId, startDate, endDate }
-  )
+  ? ApiService.post('/api/order/excel/', { storeId, startDate, endDate })
   : ApiService.post('/api/order/excel/', { storeId }));
